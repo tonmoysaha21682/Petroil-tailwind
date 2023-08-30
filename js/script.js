8d95633e6a2dd7d93630dbdbca600a110fbc674c
tonmoysaha21682 <125676915+tonmoysaha21682@users.noreply.github.com>
2023-08-30 13:37:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        width: {
+          'container': '1144px',
+       },
+       fontFamily: {
+        pops: [ 'Poppins', 'sans-serif'],
+       },
+       backgroundImage: {
+        'banner': "url('./images/banner.png')",
+        '4rthbg': "url('./images/4rthpart.png')",
+        '5thbg1': "url('./images/5th1.png')",
+        '5thbg2': "url('./images/5th2.png')",
+        '6th': "url('./images/6th.png')",
+        '8thpart1': "url('./images/8thpart1.png')",
+        '8thpart2': "url('./images/8thpart2.png')",
+        '8thpart3': "url('./images/8thpart3.png')",
+
+      }
+       
+    }
+  }
+}
